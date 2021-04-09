cff1401dc8ba361f448013e95b3e6b9bbf745c01
ccelia99 <serja.aaltonen@gmail.com>
2021-04-09 20:48:36 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
 app.use(express.json())
+app.use(cors())
 
 const morgan = require('morgan')
 morgan.token('body', function (req, res) {
@@ -103,7 +106,7 @@ app.post('/api/persons', (request, response) => {
     response.json(note)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
 })
