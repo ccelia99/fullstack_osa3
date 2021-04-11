dee42e99ee30a3713d3561513908b662c0e1ddba
ccelia99 <serja.aaltonen@gmail.com>
2021-04-11 23:21:20 +0300

    Teht. 3.22 valmiina

--- index.js
+++ index.js
@@ -9,110 +9,107 @@ app.use(cors())
 app.use(express.json())
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-    next(error)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
 }
-  
+
 const morgan = require('morgan')
-morgan.token('body', function (req, res) {
-    return JSON.stringify(req.body)
+morgan.token('body', function (req) {
+  return JSON.stringify(req.body)
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (request, response, next) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-    })
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
     .catch(error => {
-        next(error)
+      next(error)
     })
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const note = new Note({
-        name: body.name,
-        number: body.number
-    })
-   
-    note.save()
+  const note = new Note({
+    name: body.name,
+    number: body.number
+  })
+
+  note.save()
     .then(savedNote => savedNote.toJSON())
     .then(savedAndFormattedNote => {
-        response.json(savedAndFormattedNote)
-      }) 
+      response.json(savedAndFormattedNote)
+    })
     .catch(error => {
-        next(error)
+      next(error)
     })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-        .then(note => {
-            if (note) {
-                response.json(note)
-            } else {
-                response.status(404).end()
-        }
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
     })
     .catch(error => {
-        next(error)
-    })       
-})
-
-app.get('/info', (request, response, next) => {
-    
-    Note.find().exec(function (err, results) {
-        const total = results.length
-        console.log('note', total)
-        const resString  = `Phonebook has info for ${total} people 
-        ${new Date()}` 
-        const print = resString.split('\n')
-        response.send(print) 
-    })
-    .catch(error => {
-        next(error)
+      next(error)
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    Note.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => {
-            next(error)
-        })
+app.get('/info', (request, response) => {
+
+  Note.find().exec(function (err, results) {
+    const total = results.length
+    console.log('note', total)
+    const resString  = `Phonebook has info for ${total} people 
+        ${new Date()}`
+    const print = resString.split('\n')
+    response.send(print)
+  })
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(
+      response.status(204).end()
+    )
+    .catch(error => {
+      next(error)
+    })
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
-  
-    const note = {
-      name: body.name,
-      number: body.number,
-    }
-  
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
-      .then(updatedNote => {
-        response.json(updatedNote)
-      })
-      .catch(error => {
-        next(error)
+  const body = request.body
+
+  const note = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
     })
-  })
+    .catch(error => {
+      next(error)
+    })
+})
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
-  
+
 // olemattomien osoitteiden käsittely
 app.use(unknownEndpoint)
 
@@ -121,5 +118,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- models/note.js
+++ models/note.js
@@ -5,25 +5,25 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(result => {
+  .then(
     console.log('connected to MongoDB')
-  })
+  )
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
 
 const personSchema = new mongoose.Schema({
   name: {
-      type: String,
-      minlength: 3,
-      required: true,
-      unique: true
-    },
+    type: String,
+    minlength: 3,
+    required: true,
+    unique: true
+  },
   number: {
-      type: String,
-      minlength: 8,
-      required: true
-    }  
+    type: String,
+    minlength: 8,
+    required: true
+  }
 })
 
 personSchema.set('toJSON', {
